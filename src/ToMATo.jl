9aeb3c59be79ad0e29dfdf8de792cdf1e262ce3e
G. Vituri <56522687+vituri@users.noreply.github.com>
2023-08-26 18:12:58 -0300

    fix modules + add functions

--- src/ToMATo.jl
+++ src/ToMATo.jl
@@ -1,5 +1,18 @@
 module ToMATo
 
-# TO DO!!
+using Distances
+using NearestNeighbors
+using Base.Threads
+using AlgebraOfGraphics
+using GLMakie
+using DataFrames
+using Graphs
+using ProgressMeter
+
+include("graph.jl");
+export proximity_graph, graph_plot;
+
+include("tomato algorithm.jl");
+export tomato, plot_births_and_deaths;
 
 end # module ToMATo
